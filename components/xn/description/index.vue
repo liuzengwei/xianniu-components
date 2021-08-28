99dec11f5778e6b4a114a9b65a3dd2972d102471
liuzengwei <604806892@qq.com>
2021-08-28 16:48:00 +0800

    update

--- components/xn/description/index.vue
+++ components/xn/description/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="desc" :class="{ 'desc-border': border }" :style="{ margin }">
     <!-- 标题 -->
-    <h1 v-if="title" class="desc-title" v-html="title" />
+    <div class="desc-header flex justify-content-between align-items-center">
+      <h1 v-if="title" class="desc-title" v-html="title" />
+      <div class="more" v-if="$slots.more">
+        <slot name="more"></slot>
+      </div>
+    </div>
     <slot name="title" />
     <el-row class="desc-row">
       <slot />
@@ -20,14 +25,7 @@ export default {
     };
   },
   props: {
-    // 数据源，监听数据重绘
-    data: {
-      type: Object,
-      required: true,
-      default() {
-        return {};
-      },
-    },
+    // // 数据源，监听数据重绘
     // 标题
     title: {
       type: String,
@@ -56,51 +54,56 @@ export default {
     },
   },
   watch: {
-    data: {
-      handler() {
-        this.$nextTick(() => {
-          // 筛选出子组件e-desc-item
-          const dataSource = this.$slots.default;
-          const dataList = [];
-          dataSource.forEach((item) => {
-            if (
-              item.componentOptions &&
-              item.componentOptions.tag === "xn-description-item"
-            ) {
-              dataList.push(item.componentInstance);
-            }
-          });
-          // 剩余span
-          let leftSpan = this.column;
-          const len = dataList.length;
-          dataList.forEach((item, index) => {
-            // 处理column与span之间的关系
-            // 剩余的列数小于设置的span数
-            const hasLeft = leftSpan <= (item.span || 1);
-            // 当前列的下一列大于了剩余span
-            const nextColumnSpan =
-              index < len - 1 && dataList[index + 1].span >= leftSpan;
-            // 是最后一行的最后一列
-            const isLast = index === len - 1;
-            if (hasLeft || nextColumnSpan || isLast) {
-              // 满足以上条件，需要自动补全span，避免最后一列出现残缺的情况
-              item.selfSpan = leftSpan;
-              leftSpan = this.column;
-            } else {
-              leftSpan -= item.span || 1;
-            }
-          });
-        });
-      },
-      deep: true,
-      immediate: true,
-    },
+    // data: {
+    //   handler() {
+    //     this.$nextTick(() => {
+    //       // 筛选出子组件e-desc-item
+    //       const dataSource = this.$slots.default;
+    //       const dataList = [];
+    //       dataSource.forEach((item) => {
+    //         if (
+    //           item.componentOptions &&
+    //           item.componentOptions.tag === "xn-description-item"
+    //         ) {
+    //           dataList.push(item.componentInstance);
+    //         }
+    //       });
+    //       // 剩余span
+    //       let leftSpan = this.column;
+    //       const len = dataList.length;
+    //       dataList.forEach((item, index) => {
+    //         // 处理column与span之间的关系
+    //         // 剩余的列数小于设置的span数
+    //         const hasLeft = leftSpan <= (item.span || 1);
+    //         // 当前列的下一列大于了剩余span
+    //         const nextColumnSpan =
+    //           index < len - 1 && dataList[index + 1].span >= leftSpan;
+    //         // 是最后一行的最后一列
+    //         const isLast = index === len - 1;
+    //         if (hasLeft || nextColumnSpan || isLast) {
+    //           // 满足以上条件，需要自动补全span，避免最后一列出现残缺的情况
+    //           item.selfSpan = leftSpan;
+    //           leftSpan = this.column;
+    //         } else {
+    //           leftSpan -= item.span || 1;
+    //         }
+    //       });
+    //     });
+    //   },
+    //   deep: true,
+    //   immediate: true,
+    // },
   },
 };
 </script>
 
 <style scoped lang="scss">
 .desc {
+  &-header{
+    padding: 10px;
+    display: flex;
+    border-bottom: 1px solid #e8eaec;
+  }
   &.desc-border {
     border: 1px solid #e6ebf5;
     border-radius: 4px;
@@ -108,9 +111,9 @@ export default {
   background-color: #fff;
 
   .desc-title {
-    padding: 10px;
+    
     margin: 0;
-    border-bottom: 1px solid #e8eaec;
+    
     color: #ff745c;
     font-weight: 700;
     font-size: 15px;
--- components/xn/descriptionItem/index.vue
+++ components/xn/descriptionItem/index.vue
@@ -1,12 +1,13 @@
 <template>
   <el-col :span="computedSpan" class="desc-item">
     <div class="desc-item-content" :class="size">
-      <label class="desc-item-label" :style="{width: labelWidth}" v-html="label" />
+      <label
+        class="desc-item-label"
+        :style="{ width: labelWidth }"
+        v-html="label"
+      />
       <div v-if="$slots" class="desc-item-value">
-        <!-- 纯文本 -->
-        <slot v-if="$slots.default && $slots.default[0].text" />
-        <!-- HTML -->
-        <slot v-else-if="$slots.content" name="content" />
+        <slot v-if="isTrue" />
         <span v-else>暂无数据</span>
       </div>
     </div>
@@ -15,90 +16,93 @@
 
 <script>
 export default {
-  name: 'XnDescriptionItem',
-  inject: ['labelWidth', 'column', 'size'],
+  name: "XnDescriptionItem",
+  inject: ["labelWidth", "column", "size"],
   props: {
     span: {
       type: [Number, String],
       required: false,
-      default: 0
+      default: 0,
     },
     label: {
       type: String,
       required: false,
-      default: ''
-    }
+      default: "",
+    },
   },
   data() {
     return {
       // 子组件自己的span
-      selfSpan: 0
-    }
+      selfSpan: 0,
+    };
   },
   computed: {
+    isTrue(){
+      return JSON.stringify(this.$slots) === '{}'
+    },
     computedSpan() {
       // 子组件自己的span，用于父组件计算修改span
       if (this.selfSpan) {
-        return 24 / this.column * this.selfSpan
+        return (24 / this.column) * this.selfSpan;
       } else if (this.span) {
-      // props传递的span
-        return 24 / this.column * this.span
+        // props传递的span
+        return (24 / this.column) * this.span;
       } else {
-      // 未传递span时，取column
-        return 24 / this.column
+        // 未传递span时，取column
+        return 24 / this.column;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.desc-item {
+  //   margin-bottom: 20px;
+  margin: 10px 0;
+  .desc-item-content {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    color: rgba(0, 0, 0, 0.65);
+    font-size: 14px;
+    line-height: 1.5;
+    width: 100%;
+    height: 100%;
+    .desc-item-label {
+      text-align: right;
+      display: inline-block;
+      flex-grow: 0;
+      flex-shrink: 0;
+      color: #606266;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 1.5;
+      height: 100%;
+      display: flex;
+      font-weight: 600;
+      justify-content: flex-end;
+      align-items: flex-start;
+    }
+    .desc-item-value {
+      flex-grow: 1;
+      overflow: hidden;
+      word-break: break-all;
+      height: 100%;
+      display: flex;
+      align-items: flex-start;
+      color: #444;
+      padding: 0 0 0 10px;
+      span {
+        color: #aaa;
+      }
+    }
+    &.small {
+      .desc-item-label,
+      .desc-item-value {
+        padding: 10px 14px;
       }
     }
   }
 }
-</script>
-
-<style scoped lang="scss">
-  .desc-item {
-    //   margin-bottom: 20px;
-    margin: 10px 0;
-    .desc-item-content {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      color: rgba(0,0,0,.65);
-      font-size: 14px;
-      line-height: 1.5;
-      width: 100%;
-      height: 100%;
-      .desc-item-label{
-          text-align: right;
-        display: inline-block;
-        flex-grow: 0;
-        flex-shrink: 0;
-        color: #606266;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 1.5;
-        height: 100%;
-        display: flex;
-        font-weight: 600;
-        justify-content: flex-end;
-        align-items: flex-start;
-      }
-      .desc-item-value{
-        flex-grow: 1;
-        overflow: hidden;
-        word-break: break-all;
-        height: 100%;
-        display: flex;
-        align-items: flex-start;
-        color: #444;
-        // padding: 0 0 0 10px;
-        span{
-          color: #aaa;
-        }
-      }
-      &.small {
-        .desc-item-label,
-        .desc-item-value {
-          padding: 10px 14px;
-        }
-      }
-    }
-  }
 </style>
